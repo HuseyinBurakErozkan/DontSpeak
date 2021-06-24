b9ab49778682f126e72ca19a97751948b17eacc1
Huseyin Burak Erozkan <101546770@student.swin.edu.au>
2021-06-25 00:42:31 +1000

    Remove commented out code

--- test/testio.js
+++ test/testio.js
@@ -109,39 +109,3 @@ describe('Room creation Events', () => {
   });
 
 });
-
-
-// describe("Room Events with 4 players", () => {
-//   beforeEach((done) => {
-
-//     // Connect 4 io clients, representing 4 players
-//     for (var i = 0; i < 4; i++) {
-//       var player = io("http://localhost:3000/", ioOptions);
-//       player.name = "player " + i;
-//     }
-
-//     connections.push(player);
-
-//     done();
-//   });
-
-//   afterEach((done) => {
-
-//     // Disconnect every connection
-//     for (var i = 0; i < 4; i++) {
-//       connections[i].disconnect();
-//       done();
-//     }
-
-//     // Set the variable to a new array as a simple way to remove all clients
-//     connections = [];
-//   });
-
-//   it("Room should be allowed to start game as there are 4 players", (done) => {
-//     // TODO: Implement functionality required for this test to pass
-//     var canStart = true;
-//     expect(canStart).to.equal(true);
-
-//     done();
-//   });
-// });
