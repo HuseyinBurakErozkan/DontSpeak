1396c5b69b60fae777539c6f12bbdf269e68a08f
Huseyin Burak Erozkan <101546770@student.swin.edu.au>
2021-07-21 22:58:50 +1000

    Minor refactoring and comments to frontend game.js

--- client/public/javascript/game.js
+++ client/public/javascript/game.js
@@ -1,7 +1,15 @@
-// Initially instruct the user on how to understand and use the UI
-flash(tutorialMsgs.intro, "information");
+// Initially instruct the user on how to understand and use the UI. Display the flash
+// message after a short delay so the user clearly see's that it's a flash message
+setTimeout(() => {
+  flash(tutorialMsgs.intro, "information");
+}, 500);
 
 var role; // Used to define what role the current player has in each round
+
+var t1CurrentPoints = 0; // Team 1's current points
+var t2CurrentPoints = 0; // Team 2's current points
+
+var toggleHelpOn = true; // Whether to display helpful hints or not. On by default
 
 /**
  * Called once players have asked to start a game and the server has responded
@@ -71,7 +79,9 @@ socket.on("update: role:", (playerRole) => {
   }
 });
 
-
+/**
+ * Called when a word has been chosen by the server
+ */
 socket.on("update: word: ", (word) => {
 
   /**
@@ -115,7 +125,20 @@ socket.on("update: word: ", (word) => {
 
 // If this player is the speaker, display a button asking them how many points they believed
 // they earned. Once they enter, emit this amount to the server
+
+/**
+ * Emitted by the sever once the round is finished. 
+ */
 socket.on("update: round over", (wordsPlayed) => {
+
+  /**
+   * Display a screen showing all the cards the speaker has looked at, so that
+   * the players and speaker can review them and input the amount of words they
+   * got right or wrong
+   */
+  var reviewScreen = $("#screen-review");
+  reviewScreen.empty(); // Empty the review screen of previous words that may have been displayed
+  changeScreen("screen-review");
   
   // Explain to the speaker what they have to do in this step
   if (role === "speaker") {
@@ -126,18 +149,7 @@ socket.on("update: round over", (wordsPlayed) => {
     flash(tutorialMsgs.roundOverOppositionInstruction, "information"); 
   }
 
-  // Stop listening to touch-related events, as the user be able to swipe without unnecessary
-  // calls being made
-  removeTouchListeners();
-
-  console.log("round over");
-  console.log(wordsPlayed);
-
-  var reviewScreen = $("#screen-review");
-  reviewScreen.empty(); // Empty the review screen of previous words that may have been displayed
-
-  changeScreen("screen-review");
-
+  // Display all the cards/words played this round
   for (var i = 0; i < wordsPlayed.length; i++) {
     var wordCard = $("<div/>")
       .addClass("word-card");
@@ -156,19 +168,22 @@ socket.on("update: round over", (wordsPlayed) => {
     }
   }
 
+  /**
+   * The speaker will have to input the amount of points they believe they scored,
+   * so display an input field and button if the player is this round's speaker
+   */
   if (role === "speaker") {
-    console.log("this client is the speaker");
-
     // Display the ui input where the user can enter the amount of points they've earned
     $("#speaker-enter-points").removeClass("--display-hidden");   
 
     var inputField = $("#input-points-earned");
     var claimPointsButton = $("#button-confirm-points");
 
+    // Speaker hits enter
     inputField.on("keydown", (e) => {
       if(e.which == 13) {
-        console.log("claiming points ", inputField.val())
-        sendClaimPoints(inputField.val());
+        // Tell the server the amount of points that the speaker has claimed they earned
+        sendPointClaim(inputField.val());
       } else {
         // If the user is reentering a value in the input field, check if the button
         // is showing the tick, indicating that the previous input was confirmed. If
@@ -181,19 +196,26 @@ socket.on("update: round over", (wordsPlayed) => {
       }
     });
 
+    // Speaker clicks the confirm button
     claimPointsButton.on("click", (e) => {
-      console.log("claiming points ", inputField.val())
-      sendClaimPoints(inputField.val());
+      // Tell the server the amount of points that the speaker has claimed they earned
+      sendPointClaim(inputField.val());
     });
   }
 });
 
-function sendClaimPoints(pointsAmt) {
-  console.log("sendClaimPoints called");
-  console.log(pointsAmt, " typeof: ", typeof(pointsAmt));
+
+/**
+ * Handle the speaker asserting the amount of points they earned for a round, by
+ * send a request to the server.
+ * @param {number} pointsAmt The amount of points the speaker claims they earned
+ */
+function sendPointClaim(pointsAmt) {
   // Only try emit if the inputted value is an integer that is above 0.
   if (/^\d+$/.test(pointsAmt)) {
     console.log("emitted");
+
+    // Send a request to the server to update the points by the amoun the player has earned
     socket.emit("request: earned points amount", pointsAmt);
 
     // Since the input is valid the request was emitted to server, toggle the confirm
@@ -201,16 +223,25 @@ function sendClaimPoints(pointsAmt) {
     var claimPointsButton = $("#button-confirm-points");
     claimPointsButton.text("✓");
     claimPointsButton.addClass("--confirmed");
-
   } else {
     flash("Error: You need to input a positive whole number. Minimum score must be 0 as you can't lose points", "error");
   }
 }
 
+/**
+ * Emitted by the server, to request all non-speaker players to confirm the amount
+ * of points the speaker claims they earned.
+ * @param {string} speaker The speaker's name
+ * @param {number} amount The amount of points the speaker claims they earned
+ */
 socket.on("request: confirm points claim", (speaker, amount) => {
 
-  // Check if the button is in the 'confirmed' state when a new request has come in, 
-  // and if so, change it.
+  /**
+   * As the speaker may input a new value after they have already requested the
+   * server to update their points, check if the confirm button is already 'toggled'.
+   * If so, ask the player to re-confirm the new point amount the speaker has claimed
+   * to earn.
+   */
   var confirmButton = $("#button-confirm-claim");
   if (confirmButton.text() === "✓") {
     confirmButton.removeClass("--confirmed");
@@ -220,13 +251,14 @@ socket.on("request: confirm points claim", (speaker, amount) => {
   // Remove and reattach the listener each time the server asks to confirm
   $("#button-confirm-claim").off("click", "**");
 
-  console.log("speaker claims to have earned: ", speaker, amount)
-
-  // Show the confirm dialog
+  // Show the confirm ui element
   $("#player-confirm-points").removeClass("--display-hidden");
-  $("#confirm-text").text(`Did ${speaker} get ${amount} points?`);
+  $("#confirm-text").text(`Did ${speaker} earn ${amount} points?`);
 
-
+  /**
+   * If the player agrees that the speaker has earned the amount of points they claimed,
+   * send a response to the server confirming it.
+   */
   $("#button-confirm-claim").on("click", (e) => {
     socket.emit("response: confirm points");
 
@@ -236,15 +268,19 @@ socket.on("request: confirm points claim", (speaker, amount) => {
   });
 });
 
-
-var t1CurrentPoints = 0;
-var t2CurrentPoints = 0;
-
+/**
+ * Emitted by the server after the speaker's score has been confirmed
+ */
 socket.on("update: points: ", (t1Points, t2Points, pointsNeeded) => {
   handlePointUpdate(t1Points, t2Points, pointsNeeded);
 });
 
-
+/**
+ *
+ * @param {number} t1Points Team 1's current points
+ * @param {number} t2Points Team 2's current points
+ * @param {number} pointsNeeded Amount of points needed to win the game
+ */
 function handlePointUpdate(t1Points, t2Points, pointsNeeded) {
   
   // Hide the ui elements that were instantiated for the previous round
@@ -284,7 +320,12 @@ function handlePointUpdate(t1Points, t2Points, pointsNeeded) {
   t2CurrentPoints = t2Points;
 }
 
-
+/**
+ * 
+ * @param {number} newPoints The team's score before the current round
+ * @param {number} prevPoints The team's score after the current round
+ * @param {number} teamNumber The team whose score is being updted - either 1 or 2 
+ */
 function updateScoreDisplay(newPoints, prevPoints, teamNumber) {
 
   var textElement = $("#t" + teamNumber + "-scores");
@@ -324,13 +365,17 @@ function updateScoreDisplay(newPoints, prevPoints, teamNumber) {
   }, 500);
 }
 
+/**
+ * Send request to the server to start another round.
+ */
 function nextRound() {
   socket.emit("request: new round");
 }
 
+/**
+ * Emitted by the server when the player's team has lost
+ */
 socket.on("update: lost", (t1Points, t2Points, pointsNeeded) => {
-  removeTouchListeners();
-
   handlePointUpdate(t1Points, t2Points, pointsNeeded);
 
   // Hide the next round button, as the game is over
@@ -339,8 +384,6 @@ socket.on("update: lost", (t1Points, t2Points, pointsNeeded) => {
 });
 
 socket.on("update: won", (t1Points, t2Points, pointsNeeded) => {
-  removeTouchListeners();
-
   handlePointUpdate(t1Points, t2Points, pointsNeeded);
 
   $("#button-next-round").addClass("--display-hidden");
@@ -352,7 +395,6 @@ socket.on("update: won", (t1Points, t2Points, pointsNeeded) => {
  * On receiving notification that the round is starting, display the timer on the ui
  */
 socket.on("update: starting", (seconds) => {
-  console.log("update: starting: seconds = " + seconds);
   showTimer(seconds);
 
   // Hide any open flash messages, so they don't cover any of the words as the user
@@ -375,6 +417,7 @@ socket.on("update: starting", (seconds) => {
     }, 1000);
   }
 });
+
 
 function showTimer(startingSeconds) {
   var secondsLeft = startingSeconds;
@@ -411,11 +454,9 @@ function showTimer(startingSeconds) {
   }, 1000);
 }
 
-
-
-var toggleHelpOn = true;
-
-
+/**
+ * This function handles toggling helpful flash messages on or off
+ */
 function toggleHelp() {
   if (toggleHelpOn) {
     $(".game-ui-help-toggle-container").addClass("--toggle-off");
